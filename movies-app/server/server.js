69a2d3a76bef2044ac054c62500ae8f7e0dd43c1
Nabhan <nabedi2006@gmail.com>
2025-06-15 22:30:36 -0400

    added port variable

--- movies-app/server/server.js
+++ movies-app/server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -35,6 +35,7 @@ app.use('/search', searchRoutes);
 
 app.use('/favorites', favoriteRoutes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
 console.log('Server is started');
 });
